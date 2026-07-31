e5237093cfdee94be2356d0756b030fe8ef82ab4
Titi-coder-developer <tinaadindu29@gmail.com>
2026-07-31 16:22:58 +0100

    localStorage

--- scripts/week2/boom.js
+++ scripts/week2/boom.js
@@ -4,27 +4,63 @@ const inputElement = document.querySelector("#favchap");
 const buttonElement = document.querySelector("button");
 const listElement = document.querySelector("#list");
 
+// Create chaptersArray
+let chaptersArray = getChapterList() || [];
+
+// Display saved chapter when the page 
+chaptersArray.forEach((chapter) => {
+    displayList(chapter);
+});
+
+// create displayList()
+function displayList(item) {
+    const li = document.createElement("li");
+    li.textContent = item;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "❌";
+
+    deleteBtn.addEventListener("click", function () {
+        listElement.removeChild(li);
+        deleteChapter(li.textContent);
+        inputElement.focus();
+    });
+
+    li.appendChild(deleteBtn);
+
+    listElement.appendChild(li);
+}
 // wait for button clicks
 buttonElement.addEventListener("click", function () {
-    // Check if the user entered something
+
     if (inputElement.value != "") {
-        // create list item and give it the value of the input
-        const li = document.createElement("li");
-        li.textContent = inputElement.value;
-        // create a button and add a click event listener
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "❌";
-        deleteBtn.addEventListener("click", function () {
-            listElement.removeChild(li);
-            inputElement.focus();
-        });
-        // add the button to the list item
-        li.appendChild(deleteBtn);
-        // OUTPUT: finally display the completed list item to the unordered list
-        listElement.appendChild(li);
-        // clear the user input field
+
+        displayList(inputElement.value);
+
+        chaptersArray.push(inputElement.value);
+
+        setChapterList();
+
         inputElement.value = "";
+
+        inputElement.focus();
     }
-    // focus the user back to the input field
-    inputElement.focus();
+
 });
+
+function setChapterList() {
+    
+    localStorage.setItem("myFavBOMList", JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem("myFavBOMList"));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+
+    setChapterList();
+}
